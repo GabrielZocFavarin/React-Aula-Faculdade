5afd1aadebf4bc57ee67ea3eb79b6f5e0d241d71
Gabriel Favarin <147266298+GabrielZocFavarin@users.noreply.github.com>
2024-05-16 11:18:21 -0300

    Add files via upload

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from "./Home";
 import Atividade1 from "./Atividade1";
 import Atividade2 from "./Atividade2";
+import Atividade3 from "./Atividade3";
 
 export default function MyRoutes() {
     return(
@@ -12,6 +13,7 @@ export default function MyRoutes() {
             <Route path="/" element={<Home />} />
             <Route path="/Atividade1" element={<Atividade1 />} />
             <Route path="/Atividade2" element={<Atividade2 />} /> 
+            <Route path="/Atividade3" element={<Atividade3 />} />
 
         </Routes>
         </BrowserRouter>
